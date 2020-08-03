e3278441d5f83b9b9121945f5097d4f6d0813dad
xiaozhouwangatmonzo <xiaozhou.wang85@gmail.com>
2020-08-03 14:24:04 +0100

    dbt config

---
+++ dbt/build_env.sh
@@ -0,0 +1,9 @@
+#Set current directory
+export CURR_DIR="$(dirname "${BASH_SOURCE[0]}")"
+export PARENT_DIR="$(cd $CURR_DIR; cd ../ && pwd)"
+
+#Execute init.sh in parent directory to build pyenv
+source $PARENT_DIR/init.sh
+
+#Move to script location and run script
+cd $PARENT_DIR/$CURR_DIR
